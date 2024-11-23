f71e78efebf57b21f88d3701920778e68e29aa4f
Lamia Rizk <lamiarizk@gmx.de>
2024-11-23 22:07:31 +0100

    bearbeitet

--- src/main.ts
+++ src/main.ts
@@ -3,33 +3,17 @@ import { IFilm, IFilmResult } from './interfaces/IFilm';
 import { IPerson, IPersonResult } from './interfaces/IPerson'
 import { IPlanet, IPlanetResult } from './interfaces/IPlanet'
 
-
-// URLs 
 const BASE_URL = "https://swapi.dev/api/"
-
 const FILM_ROUTE = `${BASE_URL}/films/`
-
 const PLANET_ROUTE = `${BASE_URL}/planets/`
-
 const PERSON_ROUTE = `${BASE_URL}/people/`
 
-// Konstanten ziehen
 const outputElement = document.getElementById("output-wrapper") as HTMLDivElement;
-console.log(outputElement);
-
 const filmElement = document.getElementById("api-films") as HTMLAnchorElement;
-console.log(filmElement);
-
 const planetElement = document.getElementById("api-planets") as HTMLAnchorElement;
-console.log(planetElement);
-
 const personElement = document.getElementById("api-people") as HTMLAnchorElement;
-console.log(personElement);
 
 // EventListener ansprechen Kategorie Filme/Callbackfunction
-// const filmElement > FILM_ROUTE
-// IFilmResult
-// const filmDiv > const displayFilm
 filmElement?.addEventListener("click", async () => {
   try {
     const response: Response = await fetch(FILM_ROUTE)
@@ -38,11 +22,12 @@ filmElement?.addEventListener("click", async () => {
     console.log(data.results);
     data.results.forEach((result: IFilmResult) => { 
       const filmDiv = document.createElement("div") as HTMLDivElement;
+      filmDiv.className = "card";
       filmDiv.innerHTML = displayFilm(result)
       outputElement.appendChild(filmDiv)
     })
   } catch (error) {
-    console.error(error);
+    console.error("Error: process failed", error);
   }
 })
 
@@ -57,9 +42,6 @@ function displayFilm(film: IFilmResult): string {
 }
 
 // EventListener ansprechen Kategorie Planets/Callbackfunction
-// const planetElement > PLANET_ROUTE
-// IPlanetResult
-// const planetDiv > const displayPlanet
 planetElement?.addEventListener("click", async () => {
   try {
     const response: Response = await fetch(PLANET_ROUTE)
@@ -67,81 +49,80 @@ planetElement?.addEventListener("click", async () => {
     outputElement.innerHTML = ""
     console.log(data.results);
     data.results.forEach((result: IPlanetResult) => { 
+      console.log(result);
+      
       const planetDiv = document.createElement("div") as HTMLDivElement;
+      planetDiv.className = "card";
       planetDiv.innerHTML = displayPlanet(result)
       outputElement.appendChild(planetDiv)
     })
   } catch (error) {
-    console.error(error);
+    console.error("Error: process failed", error);
   }
 })
 
 // function: Planets anzeigen lassen
 function displayPlanet(planet: IPlanetResult): string {
-  // erst hier beim Funktionsaufruf wird x mit Inhalt befüllt (planet.residents)
-  showResidents(planet.residents);
   const resultAsString = `
     <h4>Name: ${planet.name}</h4>
-    <p>Residents: ${planet.residents}</p>
     <p>Terrain: ${planet.terrain}</p>
+    <p>Climate: ${planet.climate}</p>
     `
-    console.log(planet.residents);
-    
   return resultAsString
 }
 
 // hier Platzhalter residents dient nur als Art Hülle, um mit der function zu arbeiten (showResidents) => planet.residents
-async function showResidents(residents: string[]): Promise<string> {
-  residents.forEach(async(resident) => {
-    console.log(resident);
-    try {
-      const response: Response = await fetch(resident) 
-      const data = await response.json()
-      console.log(data);
-      
-
-    }
-    catch(error) {
-    console.error(error);
-    }
-  })
-}
-
+// async function showResidents(residents: string[]): Promise<string> {
+   // erst hier beim Funktionsaufruf wird x mit Inhalt befüllt (planet.residents)
+  // showResidents(planet.residents);
+//   residents.forEach(async(resident) => {
+//     console.log(resident);
+//     try {
+//       const response: Response = await fetch(resident) 
+//       const data = await response.json()
+//       console.log(data);
+//       return data.name
+//     }
+//     catch(error) {
+//     console.error(error);
+//     }
+//   })
+// }
 
 // EventListener ansprechen Kategorie People/Callbackfunction
-// const peopleElement > PERSON_ROUTE
-// IPersonResult
-// const personDiv > const displayPerson
 personElement?.addEventListener("click", async () => {
   try {
     const response: Response = await fetch(PERSON_ROUTE)
     const data = await response.json()
     outputElement.innerHTML = ""
     console.log(data.results);
-    data.results.forEach((result: IPersonResult) => { 
+    data.results.forEach(async(result: IPersonResult) => { 
       const personDiv = document.createElement("div") as HTMLDivElement;
-      personDiv.innerHTML = displayPerson(result)
+      personDiv.className = "card";
+      personDiv.innerHTML = await displayPerson(result)
       outputElement.appendChild(personDiv)
     })
   } catch (error) {
-    console.error(error);
+    console.error("Error: process failed", error);
   }
 })
 
 // function: People anzeigen lassen
-function displayPerson(person: IPersonResult): string {
+async function displayPerson(person: IPersonResult): Promise<string> {
+  const homeworldVariable = await fetchHomeworld(person.homeworld)
   const resultAsString = `
     <h4>Name: ${person.name}</h4>
-    // species ist link (string)
-    <p>Species: ${person.species}</p>
     <p>Gender: ${person.gender}</p>
     <p>Birth Year: ${person.birth_year}</p>
-    // homeworld ist link (string)
-    <p>Homeworld: ${person.homeworld}</p>
+    <p>Homeworld: ${homeworldVariable}</p>
     `
   return resultAsString
 }
-
-
-
-
+// immer wenn ich eine async-function schreibe, brauche ich zwingend den Type Promise! Hier ist es: Promise<string>
+async function fetchHomeworld(url: string): Promise<string> {
+  const response: Response = await fetch(url)
+  const responseData = await response.json()
+  console.log(responseData);
+  
+  return responseData.name
+} 
